0bf325159bc68b13853f586a171b3915d827594f
ariaandika <andikaakun17@gmail.com>
2025-02-05 13:32:25 +0700

    move drm scanner as internal package

--- drm_scanner/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod drm_scanner;
-pub mod display_info;
-mod crtc_mapper;
-mod connector_scanner;
-
-pub use drm_scanner::{DrmScanEvent, DrmScanResult, DrmScanner};
-pub use crtc_mapper::{CrtcMapper, SimpleCrtcMapper};
-pub use connector_scanner::{ConnectorScanEvent, ConnectorScanResult, ConnectorScanner};
-
