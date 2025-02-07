c00c6ae884a7373eb43e0c1b61600d5d6552757e
ariaandika <andikaakun17@gmail.com>
2025-02-07 11:31:46 +0700

    fix: open without seat as default

--- examples/raw_input/src/main.rs
+++ examples/raw_input/src/main.rs
@@ -1,39 +1,38 @@
-//! note that user must in `input` group
-//!
-//! arch linux: `sudo gpasswd -a [user] input`
-use input::{Libinput, LibinputInterface};
+use input::{event::{keyboard::KeyboardEventTrait, EventTrait}, Libinput, LibinputInterface};
 use libc::{O_RDONLY, O_RDWR, O_WRONLY};
 use std::{
-    fs::{File, OpenOptions},
-    os::{fd::AsFd, unix::{fs::OpenOptionsExt, prelude::OwnedFd}},
+    env::var,
+    fs::File,
+    os::{
+        fd::AsFd,
+        unix::{fs::OpenOptionsExt, prelude::OwnedFd},
+    },
     path::Path,
 };
 
 
-
 struct Interface {
-    session: libseat::Seat,
-    // devices: Vec<Device>
-}
-
-impl Interface {
-    fn new() -> Self {
-        let session  = libseat::Seat::open(|_,_|{}).unwrap();
-        Self { session }
-    }
+    seat: Option<libseat::Seat>,
 }
 
 impl LibinputInterface for Interface {
     fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
-        let device = self.session.open_device(&path).unwrap();
-        device.as_fd().try_clone_to_owned().map_err(|err|err.raw_os_error().unwrap())
-        // OpenOptions::new()
-        //     .custom_flags(flags)
-        //     .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
-        //     .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
-        //     .open(path)
-        //     .map(Into::into)
-        //     .map_err(|err|err.raw_os_error().unwrap())
+        match &mut self.seat {
+            // open device manually
+            None => File::options()
+                .custom_flags(flags)
+                .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
+                .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
+                .open(path)
+                .map(Into::into),
+            // open device with seat
+            Some(seat) => seat
+                .open_device(&path)
+                .unwrap()
+                .as_fd()
+                .try_clone_to_owned(),
+        }
+        .map_err(|err|err.raw_os_error().unwrap())
     }
 
     fn close_restricted(&mut self, fd: OwnedFd) {
@@ -42,19 +41,36 @@ impl LibinputInterface for Interface {
 }
 
 fn main() {
-    let mut input = Libinput::new_with_udev(Interface::new());
-    input.udev_assign_seat("seat0").unwrap();
-    use std::thread;
+    // opening device manually require user to be in `input` group
+    //
+    // using `libseat` allow user to bypass it, but it will take over
+    // the seat which presumably used by compositor to render the display
+    // and prevent it for doing so
+    let seat = if matches!(var("USE_SEAT").as_deref(),Ok("1")) {
+        let mut seat = libseat::Seat::open(|_,_|{}).unwrap();
+        println!("using seat: {:?}",seat.name());
+        Some(seat)
+    } else {
+        None
+    };
 
-    thread::spawn(||{
-        thread::sleep(std::time::Duration::from_secs(4));
-        std::process::exit(1);
-    });
+    let interface = Interface { seat };
+    let mut input = Libinput::new_with_udev(interface);
+    input.udev_assign_seat("seat0").unwrap();
 
     loop {
         input.dispatch().unwrap();
         for event in &mut input {
-            println!("Event {event:?}");
+            match event {
+                input::Event::Keyboard(event) => {
+                    println!(
+                        "Keyboard key: {:?} {:?}",
+                        event.key(),
+                        event.key_state(),
+                    );
+                }
+                event => println!("Event {event:?}")
+            }
         }
     }
 }
