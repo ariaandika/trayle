6d81f67a8d9c09488f4bcfa517e4e581b403c017
ariaandika <andikaakun17@gmail.com>
2025-02-07 13:38:10 +0700

    rm raw_input example with session

--- examples/raw_input/src/main.rs
+++ examples/raw_input/src/main.rs
@@ -10,29 +10,17 @@ use std::{
     path::Path,
 };
 
-
-struct Interface {
-    seat: Option<libseat::Seat>,
-}
+struct Interface;
 
 impl LibinputInterface for Interface {
     fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<OwnedFd, i32> {
-        match &mut self.seat {
-            // open device manually
-            None => File::options()
+        File::options()
                 .custom_flags(flags)
                 .read((flags & O_RDONLY != 0) | (flags & O_RDWR != 0))
                 .write((flags & O_WRONLY != 0) | (flags & O_RDWR != 0))
                 .open(path)
-                .map(Into::into),
-            // open device with seat
-            Some(seat) => seat
-                .open_device(&path)
-                .unwrap()
-                .as_fd()
-                .try_clone_to_owned(),
-        }
-        .map_err(|err|err.raw_os_error().unwrap())
+                .map(Into::into)
+                .map_err(|err|err.raw_os_error().unwrap())
     }
 
     fn close_restricted(&mut self, fd: OwnedFd) {
@@ -46,16 +34,7 @@ fn main() {
     // using `libseat` allow user to bypass it, but it will take over
     // the seat which presumably used by compositor to render the display
     // and prevent it for doing so
-    let seat = if matches!(var("USE_SEAT").as_deref(),Ok("1")) {
-        let mut seat = libseat::Seat::open(|_,_|{}).unwrap();
-        println!("using seat: {:?}",seat.name());
-        Some(seat)
-    } else {
-        None
-    };
-
-    let interface = Interface { seat };
-    let mut input = Libinput::new_with_udev(interface);
+    let mut input = Libinput::new_with_udev(Interface);
     input.udev_assign_seat("seat0").unwrap();
 
     loop {
