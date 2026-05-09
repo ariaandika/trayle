ed3535cfa081af4f4ad9b33932f443a865444312
ariaandika <andikaakun17@gmail.com>
2026-05-09 22:04:15 +0700

    remove socket file on exit

--- src/bin/single_client.rs
+++ src/bin/single_client.rs
@@ -4,10 +4,13 @@ use std::io;
 use todex::client::Client;
 use todex::wayland::Id;
 
+const SOCKET: &str = "/tmp/wayland-2";
 
 fn main() -> io::Result<()> {
-    let _ = std::fs::remove_file("/tmp/wayland-2");
-    let listener = UnixListener::bind("/tmp/wayland-2")?;
+    unsafe { libc::signal(libc::SIGINT, sigint as *const () as usize)};
+    let _guard = SocketGuard;
+
+    let listener = UnixListener::bind(SOCKET)?;
 
     let (stream, _) = listener.accept()?;
     let mut client = Client::new(stream);
@@ -20,3 +23,15 @@ fn main() -> io::Result<()> {
     client.flush()
 }
 
+struct SocketGuard;
+
+impl Drop for SocketGuard {
+    fn drop(&mut self) {
+        let _ = std::fs::remove_file(SOCKET);
+    }
+}
+
+fn sigint() {
+    let _ = std::fs::remove_file(SOCKET);
+    std::process::exit(0);
+}
