c466f363bb47894b94eea15e095d6de7cd15a3db
ariaandika <andikaakun17@gmail.com>
2026-05-09 20:27:42 +0700

    add wl_display and Write trait

---
+++ src/wayland/id.rs
@@ -0,0 +1,44 @@
+use std::num::NonZeroU32;
+
+/// Object ID.
+///
+/// The IDs are allocated by the entity creating the object (either client or server). IDs allocated
+/// by the client are in the range `[1, 0xfeffffff]` while IDs allocated by the server are in the
+/// range `[0xff000000, 0xffffffff]`.
+///
+/// The `0` ID is reserved to represent a null or non-existent object.
+///
+/// For efficiency purposes, the IDs are densely packed in the sense that the ID `N` will not be
+/// used until `N-1` has been used. This ordering is not merely a guideline, but a strict
+/// requirement, and there are implementations of the protocol that rigorously enforce this rule,
+/// including the ubiquitous libwayland.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct Id(NonZeroU32);
+
+impl Id {
+    pub const fn new(id: u32) -> Option<Self> {
+        debug_assert!(id > 1);
+        match NonZeroU32::new(id) {
+            Some(x) => Some(Self(x)),
+            None => None,
+        }
+    }
+
+    pub const fn new_const<const ID: u32>() -> Self {
+        debug_assert!(ID > 1);
+        unsafe { Self(NonZeroU32::new_unchecked(ID)) }
+    }
+
+    /// Returns ID as `u32`.
+    pub const fn as_u32(&self) -> u32 {
+        self.0.get()
+    }
+}
+
+impl PartialEq<u32> for Id {
+    fn eq(&self, other: &u32) -> bool {
+        self.0.get() == *other
+    }
+}
+
---
+++ src/wayland/mod.rs
@@ -0,0 +1,29 @@
+pub use wl_display::WlDisplay;
+
+mod id;
+mod wl_display;
+
+/// Writable type.
+///
+/// # Safety
+///
+/// Implementor must ensure that the pointer returned from [`spare`] is valid for write until given
+/// length.
+///
+/// [`spare`]: Self::spare
+pub unsafe trait Write {
+    /// Returns writable memory.
+    ///
+    /// # Safety
+    ///
+    /// Caller must initialize the returned pointer until `len` bytes.
+    unsafe fn spare(&mut self, len: usize) -> *mut u8;
+}
+
+// ===== blanket implementation =====
+
+unsafe impl<W: Write> Write for &mut W {
+    unsafe fn spare(&mut self, len: usize) -> *mut u8 {
+        unsafe { W::spare(self, len) }
+    }
+}
