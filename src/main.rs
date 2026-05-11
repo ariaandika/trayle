15722136127f784c54cc0bede8fd628e867f337d
ariaandika <andikaakun17@gmail.com>
2026-05-11 07:04:28 +0700

    remove EventLoop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use std::os::unix::net::{UnixListener, UnixStream};
+use std::task::Poll::{self, *};
 
-use event_loop::{EventKind, EventLoop};
+use epoll::Epoll;
 use error::Result;
+use sigfd::Sigfd;
 
 mod net;
 mod epoll;
@@ -9,45 +12,98 @@ mod sigfd;
 mod wayland;
 mod client;
 mod clients;
-mod event_loop;
 
 mod error;
 
 const SOCKET: &str = "/tmp/wayland-2";
 
+const LISTENER_KEY: u64 = 0;
+const SIGFD_KEY: u64 = 1;
+const KEY_OFFSET: u64 = 2;
+
 fn main() -> error::Terminate {
-    main2().into()
+    event_loop().into()
 }
 
-fn main2() -> Result<()> {
-    let mut event_loop = EventLoop::new(SOCKET)?;
+fn event_loop() -> Result<()> {
+    // ===== setup =====
 
-    while let Some(event) = event_loop.next_event()? {
-        match event {
-            EventKind::New(stream) => {
-                println!("[CLIENT]: new");
-                if let Err(err) = io::Write::write_all(stream, b"lmao") {
-                    eprintln!("cannot write: {err}");
-                }
+    let _guard = DropGuard;
+    let mut listener = UnixListener::bind(SOCKET)?;
+    let sigfd = Sigfd::new()?;
+    let mut epoll = Epoll::new()?;
+
+    listener.set_nonblocking(true)?;
+    epoll.add_read_interest(LISTENER_KEY, &listener)?;
+    epoll.add_read_interest(SIGFD_KEY, &sigfd)?;
+
+    let mut streams = Vec::with_capacity(8);
+
+    // ===== event loop =====
+
+    loop {
+        let Some((key, interest)) = epoll.next_event() else {
+            eprintln!("[EPOLL] blocking");
+            epoll.wait()?;
+            eprintln!("[EPOLL] complete");
+            continue;
+        };
+        match key {
+            LISTENER_KEY => match new_client(&mut listener, &mut streams, &epoll) {
+                Ready(Ok(())) => println!("[CLIENT] new"),
+                Ready(Err(err)) => eprintln!("[CLIENT] cannot create: {err}"),
+                Pending => {}
+            },
+            SIGFD_KEY => {
+                sigfd.read()?;
+                break;
             }
-            EventKind::ReadWrite(stream, interest) => {
-                print!("[CLIENT({interest:?})]: ");
-                if interest.is_read() {
+            key => {
+                if interest.is_close() {
+                    let stream = streams.swap_remove((key - KEY_OFFSET) as usize);
+                    if let Err(err) = epoll.remove_interest(&stream) {
+                        eprintln!("cannot remove epoll interest: {err}");
+                    }
+                    println!("[CLIENT]: close");
+                } else {
+                    let stream = &mut streams[(key - KEY_OFFSET) as usize];
                     let mut buf = [0; 1024];
                     let len = io::Read::read(stream, &mut buf)?;
-                    print!("{:?}", str::from_utf8(&buf[..len]));
-                    if let Err(err) = io::Write::write_all(stream, &[b'?'; 4]) {
-                        eprintln!("cannot write: {err}");
-                    }
+                    println!("[CLIENT]: {:?}", str::from_utf8(&buf[..len]));
                 }
-                println!();
-            }
-            EventKind::Close(mut stream, interest) => {
-                println!("[CLIENT({interest:?})]: close");
-                let _ = io::Write::write_all(&mut stream, b"bye bye");
             }
         }
     }
 
     Ok(())
+}
+
+fn new_client(
+    listener: &mut UnixListener,
+    streams: &mut Vec<UnixStream>,
+    epoll: &Epoll,
+) -> Poll<Result<()>> {
+    let stream = match listener.accept() {
+        Ok((stream, _)) => stream,
+        Err(err) => {
+            return match err.kind() {
+                io::ErrorKind::WouldBlock => Pending,
+                _ => return Ready(Err(err.into())),
+            };
+        }
+    };
+
+    stream.set_nonblocking(true)?;
+    epoll.add_read_interest(streams.len() as u64 + KEY_OFFSET, &stream)?;
+    streams.push(stream);
+
+    Ready(Ok(()))
+}
+
+struct DropGuard;
+
+impl Drop for DropGuard {
+    fn drop(&mut self) {
+        let _ = std::fs::remove_file(SOCKET);
+    }
 }
