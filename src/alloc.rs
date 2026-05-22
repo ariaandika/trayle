fa98161c9bb8443a69377b96aeece5e17fd830e9
ariaandika <andikaakun17@gmail.com>
2026-05-23 06:23:25 +0700

    add exponential grow method in alloc

--- src/alloc.rs
+++ src/alloc.rs
@@ -19,35 +19,39 @@ pub fn allocate<T>(cap: u32) -> NonNull<T> {
 #[cold]
 #[inline(never)]
 #[must_use]
-pub fn grow<T>(ptr: &mut NonNull<T>, cap: u32, additional: u32) -> u32 {
-    let cap = cap as usize;
-    let exp_cap = cap * 2;
-    let new_cap = exp_cap.max(cap + additional as usize);
-    let Ok(new_cap) = u32::try_from(new_cap) else {
+pub fn grow_exp<T>(ptr: &mut NonNull<T>, cap: u32) -> u32 {
+    let Ok(new_cap) = u32::try_from(cap as usize * 2) else {
         panic!("capacity overflow")
     };
-    let layout = layout::<T>(new_cap);
-    match unsafe {
-        NonNull::new(alloc::realloc(
-            ptr.as_ptr().cast(),
-            layout,
-            new_cap as usize,
-        ))
-    } {
-        Some(ok) => *ptr = ok.cast(),
-        None => alloc::handle_alloc_error(layout),
-    };
-
+    *ptr = grow_inner(*ptr, cap, new_cap);
     new_cap
 }
 
-pub fn deallocate_offset<T>(ptr: NonNull<T>, cap: u32, off: u32) {
-    unsafe {
-        alloc::dealloc(
-            ptr.as_ptr().sub(off as usize).cast(),
-            layout::<T>(cap + off),
-        )
+#[cold]
+#[inline(never)]
+#[must_use]
+pub fn grow<T>(ptr: &mut NonNull<T>, cap: u32, additional: u32) -> u32 {
+    let old_cap = cap as usize;
+    let exp_cap = old_cap * 2;
+    let new_cap = exp_cap.max(old_cap + additional as usize);
+    let Ok(new_cap) = u32::try_from(new_cap) else {
+        panic!("capacity overflow")
     };
+    *ptr = grow_inner(*ptr, cap, new_cap);
+    new_cap
+}
+
+fn grow_inner<T>(ptr: NonNull<T>, old_cap: u32, new_cap: u32) -> NonNull<T> {
+    let old_layout = layout::<T>(old_cap);
+    let result = unsafe { alloc::realloc(ptr.as_ptr().cast(), old_layout, new_cap as usize) };
+    match NonNull::new(result) {
+        Some(ok) => ok.cast(),
+        None => alloc::handle_alloc_error(old_layout),
+    }
+}
+
+pub fn deallocate_offset<T>(ptr: NonNull<T>, cap: u32, off: u32) {
+    unsafe { deallocate(ptr.sub(off as usize), cap + off) };
 }
 
 pub fn deallocate<T>(ptr: NonNull<T>, cap: u32) {
