4ebcf5dc2cdc6f626ddcbc034f1e2ceea4d2bcc5
ariaandika <andikaakun17@gmail.com>
2026-06-02 20:04:30 +0700

    unified disconnect logic

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,9 @@ use torio::log;
 
 const SOCKET_PATH: SocketPath = SocketPath::new(c"/tmp/wayland-2");
 
-const STATIC_ID_MASK: u64 = i64::MIN as u64;
-const LISTENER_ID: u64 = STATIC_ID_MASK | 1;
-const SIGFD_ID: u64 = STATIC_ID_MASK | 2;
-
-pub struct FatalError;
-
-impl<E: std::fmt::Display> From<E> for FatalError {
-    fn from(value: E) -> Self {
-        log::error!(setup, "{value}");
-        Self
-    }
-}
+const STATIC_KEY_MASK: u64 = i64::MIN as u64;
+const LISTENER_KEY: u64 = STATIC_KEY_MASK | 1;
+const SIGFD_KEY: u64 = STATIC_KEY_MASK | 2;
 
 fn main() -> ExitCode {
     let _guard = log::init();
@@ -41,8 +32,8 @@ fn event_loop() -> Result<(), FatalError> {
 
     let mut events = EventSources::new()?;
 
-    events.add(LISTENER_ID, &listener);
-    events.add(SIGFD_ID, &sigfd);
+    events.add(LISTENER_KEY, &listener);
+    events.add(SIGFD_KEY, &sigfd);
 
     // ===== event loop =====
 
@@ -54,48 +45,35 @@ fn event_loop() -> Result<(), FatalError> {
             continue;
         };
 
-        if key & STATIC_ID_MASK == STATIC_ID_MASK {
-            match key {
-                LISTENER_ID => loop {
-                    let conn = match listener.poll_accept() {
-                        Ready(Ok(ok)) => ok,
-                        Ready(Err(err)) => {
-                            log::error!(listener, "{err}");
-                            break;
-                        }
-                        Pending => break,
-                    };
-                    let (id, client) = clients.insert(conn);
-                    events.add(id.to_u64(), client);
-                    log::debug!(client, "id={id} connected");
-                },
-                SIGFD_ID => {
-                    let sig = sigfd.read();
-                    log::info!(sigfd, "{sig} signal received");
-                    break;
-                },
-                _ => log::error!(epoll, "unknown static key: {key}"),
-            }
-            continue;
+        if key == SIGFD_KEY {
+            log::info!(sigfd, "{} signal received", sigfd.read());
+            break;
         }
 
-        let id = ClientId::from_u64(key);
-
-        if interest.is_close() {
-            let Some(client) = clients.remove(id) else {
-                log::warn!(epoll, "unknown dynamic key: {id}");
-                continue;
-            };
-            events.delete(&client);
-            log::debug!(client, "id={id} disconnected (hup)");
+        if key == LISTENER_KEY {
+            while let Ready(result) = listener.poll_accept() {
+                match result {
+                    Ok(fd) => {
+                        let (id, client) = clients.insert(fd);
+                        events.add(id.to_u64(), client);
+                        log::debug!(client, "id={id} connected");
+                    }
+                    Err(err) => {
+                        log::error!(listener, "{err}");
+                        break;
+                    }
+                }
+            }
             continue;
         }
 
         debug_assert!(read_buf.is_empty());
         debug_assert!(write_buf.is_empty());
 
+        let id = ClientId::from_u64(key);
+
         let Some(client) = clients.get_mut(id) else {
-            log::warn!(epoll, "unknown dynamic key: {id}");
+            log::warn!(epoll, "unknown client id from event key: {id}");
             continue;
         };
 
@@ -103,17 +81,19 @@ fn event_loop() -> Result<(), FatalError> {
 
         // cope and seeth: https://github.com/rust-lang/rust/issues/31436
         let result = (|| {
+            if interest.is_close() {
+                return Err(HandleError);
+            }
+
             if interest.is_read() {
                 let mut client = ClientMut::new(client, &mut write_buf);
                 loop {
                     if read_buf.has_frame() {
-                        if router(&mut read_buf, &mut client, &mut compositor).is_err() {
-                            return Err(FatalError);
-                        }
+                        router(&mut read_buf, &mut client, &mut compositor)
+                            .inspect_err(|e| client.send_global_error(*e))?;
                     } else {
-                        match read_buf.recvmsg(&client)? {
-                            Ready(()) => continue,
-                            Pending => break,
+                        if read_buf.recvmsg(&client)?.is_pending() {
+                            break;
                         }
                     }
                 }
@@ -134,29 +114,28 @@ fn event_loop() -> Result<(), FatalError> {
                 }
             }
 
-            Ok::<_, FatalError>(())
+            Ok::<_, HandleError>(())
         })();
 
-        match result {
-            Ok(()) => {
-                if !read_buf.is_empty() || !write_buf.is_empty() {
-                    log::warn!(client, "partial message read: {}, write: {}", read_buf.len(), write_buf.len());
-
-                    // the sad pending bytes cannot stay in shared buffer because it will be used for other
-                    // socket, it will be stored in on demand allocation
-                    client
-                        .buffer_mut()
-                        .copy_from(&read_buf, &write_buf);
-                }
-            },
-            Err(_) => {
-                if !write_buf.is_empty() {
-                    let _ = write_buf.sendmsg(client);
-                }
-                events.delete(client);
-                clients.remove(id);
-                log::debug!(client, "id={id} disconnected");
-            },
+        if result.is_ok() {
+            if !read_buf.is_empty() || !write_buf.is_empty() {
+                log::warn!(
+                    client,
+                    "partial message read: {}, write: {}",
+                    read_buf.len(),
+                    write_buf.len()
+                );
+                // the sad pending bytes cannot stay in shared buffer because it will be used for other
+                // socket, it will be stored in on demand allocation
+                client.buffer_mut().copy_from(&read_buf, &write_buf);
+            }
+        } else {
+            if !write_buf.is_empty() {
+                let _ = write_buf.sendmsg(client);
+            }
+            events.delete(client);
+            clients.remove(id);
+            log::debug!(client, "id={id} disconnected");
         }
 
         read_buf.clear();
@@ -170,8 +149,6 @@ fn event_loop() -> Result<(), FatalError> {
 
 use torio::wayland::{Decode, Frame, Interface, MessageBuf, OpCode, WlError};
 
-const NOVALUE: usize = 0;
-
 static GLOBALS: [(&str, u32, Interface); 5] = [
     ("wl_compositor", 7, Interface::WlCompositor),
     ("wl_shm", 2, Interface::WlShm),
@@ -181,25 +158,10 @@ static GLOBALS: [(&str, u32, Interface); 5] = [
 ];
 
 pub struct Compositor {
-    pub seat: Seat,
+    seat: Seat,
 }
 
 pub fn router(
-    read_buf: &mut MessageBuf,
-    client: &mut ClientMut,
-    compositor: &mut Compositor,
-) -> Result<(), WlError> {
-    match router_inner(read_buf, client, compositor) {
-        Ok(()) => Ok(()),
-        Err(error) => {
-            log::error!(client, "{error}");
-            client.send_global_error(error);
-            Err(error)
-        }
-    }
-}
-
-fn router_inner(
     read_buf: &mut MessageBuf,
     client: &mut ClientMut,
     compositor: &mut Compositor,
@@ -332,7 +294,7 @@ mod wl_registry {
             if bind.id_version > *version {
                 return Err(WlError::UnknownBind);
             }
-            client.objects_mut().insert_with(bind.id, *iface, NOVALUE)?;
+            client.objects_mut().insert_with(bind.id, *iface, 0)?;
 
             // some interface has side-effect after binding
             if let Interface::WlSeat = iface {
@@ -393,5 +355,41 @@ mod wl_data_device_manager {
             };
             client.insert(&data_device)
         }
+    }
+}
+
+// ===== Errors =====
+
+struct HandleError;
+
+impl From<torio::wayland::buffer::ReadError> for HandleError {
+    fn from(err: torio::wayland::buffer::ReadError) -> Self {
+        if !err.is_connection_aborted() {
+            log::error!(client, "failed to read socket: {err}");
+        }
+        Self
+    }
+}
+
+impl From<torio::wayland::buffer::WriteError> for HandleError {
+    fn from(err: torio::wayland::buffer::WriteError) -> Self {
+        log::error!(client, "failed to write socket: {err}");
+        Self
+    }
+}
+
+impl From<WlError> for HandleError {
+    fn from(err: WlError) -> Self {
+        log::error!(client, "failed to handle request: {err}");
+        Self
+    }
+}
+
+pub struct FatalError;
+
+impl<E: std::fmt::Display> From<E> for FatalError {
+    fn from(value: E) -> Self {
+        log::error!(setup, "{value}");
+        Self
     }
 }
