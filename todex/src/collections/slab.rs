3e1a6ff15e334c687d572f60a23249499962ca22
ariaandika <andikaakun17@gmail.com>
2026-07-12 18:36:20 +0700

    add iterator over Slab

--- todex/src/collections/slab.rs
+++ todex/src/collections/slab.rs
@@ -1,6 +1,8 @@
 use core::hint;
 use core::mem;
 use core::ptr::{self, NonNull};
+use core::slice;
+use std::fmt;
 
 use crate::alloc;
 
@@ -90,6 +92,10 @@ impl<T> Slab<T> {
 }
 
 impl<T> Slab<T> {
+    fn as_slice(&self) -> &[Entry<T>] {
+        unsafe { slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+    }
+
     /// Returns shared reference of an element with associated `key`.
     #[inline]
     pub fn get(&self, key: usize) -> Option<&T> {
@@ -143,5 +149,53 @@ impl<T> Slab<T> {
                 None
             }
         }
+    }
+
+    /// Returns an iterator over the slab.
+    #[inline]
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter(self.as_slice().iter().filter_map(|e| match e {
+            Entry::Some(e) => Some(e),
+            Entry::None(_) => None,
+        }))
+    }
+}
+
+impl<T: fmt::Debug> fmt::Debug for Slab<T> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self).finish()
+    }
+}
+
+// ===== Iterator =====
+
+pub struct Iter<'a, T>(IntoIter<'a, T>);
+
+type IntoIter<'a, T> =
+    std::iter::FilterMap<std::slice::Iter<'a, Entry<T>>, fn(&'a Entry<T>) -> Option<&'a T>>;
+
+impl<'a, T> IntoIterator for &'a Slab<T> {
+    type Item = &'a T;
+
+    type IntoIter = Iter<'a, T>;
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
     }
 }
